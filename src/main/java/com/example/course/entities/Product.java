b33298c6476f5249d2e9c56be35c794f14ff2a7f
Ana Lívia Meira <analiviameira@gmail.com>
2022-08-10 07:23:10 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_Product")
@@ -32,6 +35,10 @@ public class Product implements Serializable{
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id")) //tabela de ligacao(nometabelaligacao, chave estrangeira do produto, chave estrangeira de categoria)
 	private Set<Category> categories = new HashSet<>();     //é importante lembrar de colocar uma ref em categories para esse mapeamento da tabela de ligacao
+	
+	//associa todas as linhas em order item que tenham o mesmo id que esse produto
+	@OneToMany(mappedBy = "id.product") //id está eh orderItem e product eh atributo do id
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Product() {
 		super();
@@ -89,6 +96,21 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	//método que vai retornar os pedidos de que esse produto faz parte
+	// mas buscando a partir da tabela de associação orderItem
+	@JsonIgnore //essa anotação impede que quando esse produto for chamada da classe order, ele chame order de novo e entre em um loop
+				//além de impedir que ao chamar product ele retorne para o serviço(postman) os pedidos de que product faz parte
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		
+		//vamos rodas a lista de associação aqui e selecionar apenas os pedidos em que esse produto esrá
+		for (OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		
+		return set;
 	}
 
 	@Override
